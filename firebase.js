a4e6ace14db1ecbd767556eaba9de31b9719e7d2
SomethingOwlish <55540709+SomethingOwlish@users.noreply.github.com>
2026-06-24 15:48:08 +0200

    start

---
+++ firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAY0qWBJI0QWJv68PVTlG8YFa_anXuQSZg",
+  authDomain: "tasktrackergando.firebaseapp.com",
+  projectId: "tasktrackergando",
+  storageBucket: "tasktrackergando.firebasestorage.app",
+  messagingSenderId: "829759743503",
+  appId: "1:829759743503:web:903d54f812bce3270b657a"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
